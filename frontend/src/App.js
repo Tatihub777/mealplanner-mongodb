4ee99a4a9081f3cb5f8ed6c13265c14f51b2c2f0
Tatjana <tanjaiv777@gmail.com>
2024-10-18 17:52:29 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+    <h1>
+Meal Plan
+
+    </h1>
+    <input type="text" placeholder="add a meal"/> 
+    <button>ADD</button>
+    <p>test</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Meal.js
@@ -0,0 +1,7 @@
+export const MyMeals = () => {
+    return (
+        <div>
+<p>My Meals</p>
+        </div>
+    )
+}
